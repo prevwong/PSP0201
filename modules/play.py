b2a45249e2877c8542b4ff5f579ba3099040d45c
Nicholas1998 <nicholasbb@outlook.com>
2017-05-01 17:29:19 +0800

    Changed the UI
    Give me sometime for the users' part

--- modules/play.py
+++ modules/play.py
@@ -8,34 +8,37 @@ LogWindow.title("AskTrivia")
 LogWindow.geometry("500x300")
 FrameColor = "light blue"
 LogWindow.configure(bg=FrameColor)
+Welcome = Label(LogWindow,text = "Welcome to\n AskTrivia",bg=FrameColor,font="Arial")
 
-LogUser = Label(LogWindow,text = "Username: ",bg=FrameColor,font="Arial").grid()
+LogUser = Label(LogWindow,text = "Username:",bg=FrameColor,font="Arial")
 Username1 = Entry(LogWindow)
-Username1.grid(row=0,column=1)
-LogPass = Label (LogWindow,text="Password: ",bg=FrameColor,font="Arial").grid()
+LogPass = Label (LogWindow,text="Password:",bg=FrameColor,font="Arial")
 Password1 = Entry(LogWindow,show = "*")
-Password1.grid(row=1,column=1)
 
 #SecondWindow
 RegWindow = Tk()
 RegWindow.title("Register")
 RegWindow.geometry("500x300")
 RegWindow.configure(bg = FrameColor)
+RegWord = Label (RegWindow,text="REGISTER",font = "Arial",bg=FrameColor)
 
-RegUser = Label (RegWindow,text="Username: ",bg=FrameColor,font="Arial").grid()
+RegUser = Label (RegWindow,text="Username: ",bg=FrameColor,font="Arial")
 Username2 = Entry(RegWindow)
-Username2.grid(row=0,column=1)
-RegPass = Label(RegWindow,text="Password: ",bg=FrameColor,font="Arial").grid()
+RegPass = Label(RegWindow,text="Password: ",bg=FrameColor,font="Arial")
 Password2 = Entry(RegWindow,show="*")
-Password2.grid(row=1,column=1)
 RegWindow.withdraw()
+
 data = 0
+users = {}
+users[0] = {}
+
 def Submit():
 	users[0]["Username"]=Username2.get()
 	users[0]["Password"]=Password2.get()
 	with open ("save.json","w") as save:
 		json.dump(users,save)
 	print users
+	tkMessageBox.showinfo("Done","Register Successfully!")
 	
 def Back():
     RegWindow.withdraw()
@@ -48,16 +51,49 @@ def Register():
 def Login(): 
 	with open ("save.json","r") as save:
 		json.load(save)
-	if users["0"]["Username"] == Username1.get() or users["0"]["Password"]==Password1.get():
-		print "yes"
+	if users[0]["Username"] == Username1.get() and users[0]["Password"]==Password1.get():
+		tkMessageBox.showinfo("Done","Login Successfully!")
+		Profile.deiconify()
+		LogWindow.withdraw()
 	else:
-		print"no"
-
-users = {}
-users[0] = {}
+		tkMessageBox.showerror("Error","Please Try Again!")
+		
+#Profile Window
+Profile = Tk()
+Profile.title("Profile")
+Profile.geometry("500x300")
+Profile.withdraw()
+#Button
 ButtonColor = "light green"
-submitButton = Button (RegWindow,text = "Submit",command=Submit).grid()
-backButton=Button(RegWindow,text ="Back",command=Back,bg=ButtonColor,activebackground = "white",activeforeground ="black").grid()
-registerButton=Button(LogWindow,text="Register",command=Register,bg=ButtonColor,activebackground = "white",activeforeground="black").grid()
-loginButton = Button(LogWindow,text = "Login",command=Login).grid()
+
+submitButton = Button (RegWindow,text = "    Submit    ",command=Submit,bg="Pink",activebackground = "white",activeforeground ="black")
+backButton=Button(RegWindow,text ="    Back    ",command=Back,bg=ButtonColor,activebackground = "white",activeforeground ="black")
+registerButton=Button(LogWindow,text="    Register    ",command=Register,bg=ButtonColor,activebackground = "white",activeforeground="black")
+loginButton = Button(LogWindow,text = "    Login    ",command=Login,bg="Pink",activebackground = "white",activeforeground="black")
+
+#Adjust Lining LogWindow
+LogSpaceX1 = Label (LogWindow,text="                                 ",bg=FrameColor).grid(row=0,column=0)
+LogSpaceY1 = Label (LogWindow,text=" ",bg=FrameColor).grid(row=1,column=0)
+LogSpaceY2 = Label (LogWindow,text=" ",bg=FrameColor).grid(row=4,column=0)
+Welcome.grid(row=0,column=4)
+LogUser.grid(row=2,column=3)
+Username1.grid(row=2,column=4)
+LogPass.grid(row=3,column=3)
+Password1.grid(row=3,column=4)
+loginButton.grid(row=5,column=3)
+registerButton.grid(row=5,column=4)
+
+#Adjust Lining RegWindow
+RegSpaceX1 = Label (RegWindow,text="                                 ",bg=FrameColor).grid(row=0,column=0)
+RegSpaceY1 = Label (RegWindow,text=" ",bg=FrameColor).grid(row=1,column=0)
+RegSpaceY2 = Label (RegWindow,text=" ",bg=FrameColor).grid(row=4,column=0)
+RegWord.grid(row=0,column=4)
+RegUser.grid(row=2,column=3)
+Username2.grid(row=2,column=4)
+RegPass.grid(row=3,column=3)
+Password2.grid(row=3,column=4)
+submitButton.grid(row=5,column=3)
+backButton.grid(row=5,column=4)
+
+
 LogWindow.mainloop()
