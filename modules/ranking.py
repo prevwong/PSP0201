7eea41f88b5c0abb00701a28c979610e11d80c32
dannyongtey <dannyongtey@gmail.com>
2017-05-01 14:30:58 +0800

    Code refactoring and added weekly reset functions
    Added general functions: read users.json and write users.json. Added
    functions to reset weekly exp and refactored code.

--- modules/ranking.py
+++ modules/ranking.py
@@ -2,17 +2,61 @@ from Tkinter import *
 import json
 import os
 import ttk
-#ui
+from datetime import datetime
+from threading import Timer
+import time
 
-def show_ranking():
-    
+
+#Sort by EXP
+def sort_exp(typeof, users):
+    sorted_array = sorted(users, key=lambda x: int((users[str(x)][typeof])), reverse=True)
+    i = 1
+    info_array = {}
+    for id in sorted_array:
+        info_array[i]={}
+        info_array[i]["name"] = users[id]["name"]
+        info_array[i][typeof] = users[id][typeof]
+        i = i + 1
+    return info_array
+
+#Open users.json
+def open_user_json():
     cur_path = os.path.dirname(__file__)
     new_path = os.path.join(cur_path, "../data/test.json")
+
+    #Initialize users json file
     with open(new_path) as infile:
         raw_file = json.load(infile)
         users = json.loads(raw_file)
+    return users
+
+#Save users.json
+def save_user_json(users):
+    cur_path = os.path.dirname(__file__)
+    new_path = os.path.join(cur_path, "../data/test.json")
+    users = json.dumps(users, sort_keys = True)
+    with open(new_path, "w") as outfile:
+        json.dump(users, outfile)
+    return 0
+
+def reset_weeklyexp():    
+    users = open_user_json()
+    for i in range(0, len(users)):
+        users[str(i)]["weeklyexp"] = 0
+    #This line is for testing purpose
+    #users["1"]["weeklyexp"] = 12   
+    save_user_json(users)
+    
+#UI
+def show_ranking():
+    
+    #Initialize variables
+    users = open_user_json() 
+    
+    ###UI rendering###
     
     root = Tk()
+    root.title("AskTrivia Leaderboard")
     tabs = ttk.Notebook(root)
     exp_frame = ttk.Frame(tabs)
     weeklyexp_frame = ttk.Frame(tabs)
@@ -34,8 +78,8 @@ def show_ranking():
     rank_text.grid(row=2,column = 0, columnspan = 2, pady = 10)
     name_text.grid(row=2,column = 2, columnspan = 2)
     exp_text.grid(row=2,column = 4, columnspan = 2)
-
-    rank_by_exp = sort_by_exp(users)
+    
+    rank_by_exp = sort_exp("exp",users)
     if len(rank_by_exp) <= 10:
         for i in range(1, len(rank_by_exp)+1):
             rank = Label(exp_frame, text = str(i)).grid(row = i + 2, column = 0, columnspan = 2)
@@ -51,7 +95,7 @@ def show_ranking():
     name_text.grid(row=2,column = 2, columnspan = 2)
     exp_text.grid(row=2,column = 4, columnspan = 2)
 
-    rank_by_exp = sort_by_weeklyexp(users)
+    rank_by_exp = sort_exp("weeklyexp",users)
     if len(rank_by_exp) <= 10:
         for i in range(1, len(rank_by_exp)+1):
             rank = Label(weeklyexp_frame, text = str(i)).grid(row = i + 2, column = 0, columnspan = 2)
@@ -61,30 +105,12 @@ def show_ranking():
 
     root.mainloop()
 
-#sort by exp
-    
-def sort_by_exp(users):
-    exp_sorted = sorted(users, key=lambda x: int((users[str(x)]["exp"])), reverse=True)
-    i = 1
-    info_array = {}
-    for id in exp_sorted:
-        info_array[i]={}
-        info_array[i]["name"] = users[id]["name"]
-        info_array[i]["exp"] = users[id]["exp"]
-        i = i + 1
-    return info_array
-    
-#sort by weekly exp
-def sort_by_weeklyexp(users):
-    weeklyexp_sorted = sorted(users, key=lambda x: int((users[str(x)]["weeklyexp"])), reverse=True)
-    i = 1
-    info_array = {}
-    for id in weeklyexp_sorted:
-        info_array[i]={}
-        info_array[i]["name"] = users[id]["name"]
-        info_array[i]["weeklyexp"] = users[id]["weeklyexp"]
-        i = i + 1
-    return info_array
-
+#The reset_at function
+def reset_at(year = 2017, month = 04, day = 30, hour = 21, minutes = 41, seconds = 0):
+    reset_time = datetime(year, month, day, hour, minutes, seconds)
+    while datetime.now() < reset_time:
+        time.sleep(1)
+    reset_weeklyexp()
 
 show_ranking()
+
