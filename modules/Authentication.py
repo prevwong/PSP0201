42333d3bead8068d1880a255033139db014e3af7
Nicholas1998 <nicholasbb@outlook.com>
2017-05-07 14:09:22 +0800

    Edited
    Import OS (added)
    Change the dir of "save.json" from modules to data

--- modules/Authentication.py
+++ modules/Authentication.py
@@ -1,7 +1,9 @@
 from Tkinter import *
 import tkMessageBox
 import json
-
+import os
+cur_path = os.path.dirname(__file__)
+new_path = os.path.join(cur_path, "../data/save.json")
 #First Window
 LogWindow = Tk()
 LogWindow.title("AskTrivia")
@@ -11,9 +13,9 @@ LogWindow.configure(bg=FrameColor)
 Welcome = Label(LogWindow,text = "Welcome to\n AskTrivia",bg=FrameColor,font="Arial")
 
 LogUser = Label(LogWindow,text = "Username:",bg=FrameColor,font="Arial")
-Username1 = Entry(LogWindow)
+Log_Username = Entry(LogWindow)
 LogPass = Label (LogWindow,text="Password:",bg=FrameColor,font="Arial")
-Password1 = Entry(LogWindow,show = "*")
+Log_Password = Entry(LogWindow,show = "*")
 
 #SecondWindow
 RegWindow = Tk()
@@ -23,51 +25,47 @@ RegWindow.configure(bg = FrameColor)
 RegWord = Label (RegWindow,text="REGISTER",font = "Arial",bg=FrameColor)
 
 RegUser = Label (RegWindow,text="Username: ",bg=FrameColor,font="Arial")
-Username2 = Entry(RegWindow)
+Reg_Username = Entry(RegWindow)
 RegPass = Label(RegWindow,text="Password: ",bg=FrameColor,font="Arial")
-Password2 = Entry(RegWindow,show="*")
-Con_Pass = Label(RegWindow,text="Password Confirmation: ",bg=FrameColor,font="Arial")
-ConPass = Entry(RegWindow,show="*")
+Reg_Password = Entry(RegWindow,show="*")
+ConfirmationPass = Label(RegWindow,text="Password Confirmation: ",bg=FrameColor,font="Arial")
+Confirmation_Pass = Entry(RegWindow,show="*")
 RegWindow.withdraw()
 users = {}
-
-
-
-                
-               
+ 
                 
 def Submit():
         
-        if len(Password2.get()) <6:
+        if len(Reg_Password.get()) <6:
                 
                 tkMessageBox.showerror("Error","Minimum length of Password is 6")
 
-        elif len(Username2.get())<6:
+        elif len(Reg_Username.get())<6:
                 tkMessageBox.showerror("Error","Minimum length of Username is 6")
                 
-        elif ConPass.get() !=Password2.get():
+        elif Confirmation_Pass.get() !=Reg_Password.get():
                 tkMessageBox.showerror("Error","Both Passwords did not match. Try Again")
         
         else:
-                a = Password2.get()
+                a = Reg_Password.get()
                 b = list(a)
                 b.reverse()
-                with open ("save.json", "r") as save:
+                with open (new_path) as save:
                         users  = json.load(save)
                 for i in range (0,len(users)):
-                        if users[str(i)]["Username"] == Username2.get():
-                                tkMessageBox.showerror("Error","Username:"+Username2.get()+" has been taken")
+                        if users[str(i)]["Username"] == Reg_Username.get():
+                                tkMessageBox.showerror("Error","Username:"+Reg_Username.get()+" has been taken")
                                 break
                         elif i == len(users)-1:
                                 new_id = len(users)
                                 users[new_id] = {}
-                                users[new_id]["Username"]=Username2.get()
+                                users[new_id]["Username"]=Reg_Username.get()
                                 users[new_id]["Password"] = "".join(b)
                                 users[new_id]["description"] = "Set Your Description"
                                 users[new_id]["exp"]=0
                                 users[new_id]["weeklyexp"]=0
                                 
-                                with open ("save.json","w") as save:
+                                with open (new_path,"w") as save:
                                         json.dump(users,save)
                                 print users
                                 tkMessageBox.showinfo("Done","Register Successfully!")
@@ -82,7 +80,7 @@ def Register():
     LogWindow.withdraw()
         
 def Login(): 
-        with open ("save.json","r") as save:
+        with open (new_path) as save:
                 users =json.load(save)
  
 
@@ -90,7 +88,7 @@ def Login():
                 c = users[str(i)]["Password"]
                 d = list(c)
                 d.reverse()
-                if users[str(i)]["Username"] == Username1.get() and "".join(d)==Password1.get():
+                if users[str(i)]["Username"] == Log_Username.get() and "".join(d)==Log_Password.get():
                         tkMessageBox.showinfo("Done","Login Successfully!")
                         Profile.deiconify()
                         LogWindow.withdraw()
@@ -117,26 +115,28 @@ LogSpaceY1 = Label (LogWindow,text=" ",bg=FrameColor).grid(row=1,column=0)
 LogSpaceY2 = Label (LogWindow,text=" ",bg=FrameColor).grid(row=4,column=0)
 Welcome.grid(row=0,column=4)
 LogUser.grid(row=2,column=3)
-Username1.grid(row=2,column=4)
+Log_Username.grid(row=2,column=4)
 LogPass.grid(row=3,column=3)
-Password1.grid(row=3,column=4)
-Con_Pass.grid(row=4,column=3)
-ConPass.grid(row=4,column=4)
+Log_Password.grid(row=3,column=4)
+
 loginButton.grid(row=5,column=3)
 registerButton.grid(row=5,column=4)
 
 
 #Adjust Lining RegWindow
-RegSpaceX1 = Label (RegWindow,text="                                 ",bg=FrameColor).grid(row=0,column=0)
+RegSpaceX1 = Label (RegWindow,text="            ",bg=FrameColor).grid(row=0,column=0)
 RegSpaceY1 = Label (RegWindow,text=" ",bg=FrameColor).grid(row=1,column=0)
 RegSpaceY2 = Label (RegWindow,text=" ",bg=FrameColor).grid(row=4,column=0)
+RegSpaceY3 = Label (RegWindow,text=" ",bg=FrameColor).grid(row=5,column=0)
 RegWord.grid(row=0,column=4)
 RegUser.grid(row=2,column=3)
-Username2.grid(row=2,column=4)
+Reg_Username.grid(row=2,column=4)
 RegPass.grid(row=3,column=3)
-Password2.grid(row=3,column=4)
-submitButton.grid(row=5,column=3)
-backButton.grid(row=5,column=4)
+Reg_Password.grid(row=3,column=4)
+ConfirmationPass.grid(row=4,column=3)
+Confirmation_Pass.grid(row=4,column=4)
+submitButton.grid(row=6,column=3)
+backButton.grid(row=6,column=4)
 
 
 
