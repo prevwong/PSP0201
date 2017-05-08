3016b4fbd42f2e563b759d9345321d89559a0b15
Prev Wong <prevwong@icloud.com>
2017-05-08 16:03:18 +0800

    Updated

--- modules/methods.py
+++ modules/methods.py
@@ -45,6 +45,29 @@ def defineWindow(title = "AskTrivia", geometry = "640x480"):
         window.geometry(geometry)
         return window
 
+def backupQuestions():
+        categorynum = {"Random":9,"Books":10,"Film":11,"Music":12,"Musicals & Theatres":13,"Television":14,"Video Games":15,"Board Games":16,
+               "Science & Nature":17,"Computers":18,"Mathematics":19,"Mythology":20,"Sports":21,"Geography":22,"History":23,"Politics":24,"Art":25,
+               "Celebrities":26,"Animals":27,"Vehicles":28,"Comics":29,"Gadgets":30,"Japanese Anime & Manga":31,"Cartoon & Animations":32}
+
+        quantities = [5, 10, 15, 20]
+
+        obj = {}
+
+        for i in categorynum:
+                category = categorynum[i]
+                obj[category] = {}
+
+                for quantity in quantities: 
+                        url = "https://opentdb.com/api.php?amount="+ str(quantity) +"&category=" + str(category)
+                        response = urllib.urlopen(url)
+                        jsonData = json.loads(response.read())
+                        results = jsonData["results"]
+                        obj[category][quantity] = results
+                        print "added", category, quantity
+
+        with open('data/backup.json', 'w') as outfile:
+                json.dump(obj, outfile)
 
 
 
